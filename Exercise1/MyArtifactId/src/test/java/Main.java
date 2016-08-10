3518bd42448c827a8bac3a5d922f36e0917529cb
UrielDanziger <UrielDanziger@gmail.com>
2016-08-10 15:50:17 +0300

    blank main menu

---
+++ Exercise1/MyArtifactId/src/test/java/Main.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+public class Main {
+        public static void main(String[] args) throws IOException {
+        	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+       	int choice = 3;
+        boolean exit = false;
+        String path;
+        
+        while (exit != true)
+        	{
+        	System.out.println("1 - for encryption");
+        	System.out.println("2 - for decryption");
+        	System.out.println("0 - for exiting");
+        	choice = Integer.parseInt(reader.readLine());
+        	try{	
+        	switch (choice)
+		       	{
+		        	case 1:
+		        	{
+		        		
+		        		//Todo encrypt
+		        		break;
+		        	}
+		        	case 2:
+		        	{
+		        		
+		        		//Todo decrypt
+		        		break;
+		        	}
+		        	case 0:
+		        	{
+		        		exit = true;
+		        		break;
+		        	}
+		        	default:
+		        	{
+		        		System.out.println("try again");
+		        		break;
+		        	}
+		       	}}
+        		
+
+        		}
+        	}
+        }
+           
+    };
